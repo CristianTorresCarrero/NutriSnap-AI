f4e6fdab23eb2580c6dfc0c282a30ad6f39679f4
CristianTorresCarrero} <cristiantorrescarrero@gmail.com>
2026-06-05 21:01:18 -0500

    feat: initialize NutriSnap AI backend project

---
+++ src/main/java/com/nutrisnap/NutrisnapApplication.java
@@ -0,0 +1,13 @@
+package com.nutrisnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrisnapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutrisnapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nutrisnap/NutrisnapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nutrisnap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrisnapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
